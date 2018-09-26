4d9cd61d5805b869a061d9be4376f35ad172e6fb
Deep <deepjparekh@gmail.com>
2018-09-26 19:09:39 -0400

    Init tests and fastlane

--- deepRed/deepRed/AppDelegate.swift
+++ deepRed/deepRed/AppDelegate.swift
@@ -15,12 +15,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        
+        #if TEST
+        window?.rootViewController = UINavigationController(rootViewController: UIViewController())
+        #else
         let appCoordinator = AppCoordinator()
         appCoordinator.start(withAnimation: true)
         guard let rootViewController = appCoordinator.viewController else {
             return false
         }
         window?.rootViewController = UINavigationController(rootViewController: rootViewController)
+        #endif
         window?.makeKeyAndVisible()
         return true
     }

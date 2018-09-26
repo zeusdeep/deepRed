8f6756f99669096dc139160bbdeccd389d093136
Deep <deepjparekh@gmail.com>
2018-09-25 21:54:25 -0400

    Initial Project Commit

---
+++ deepRed/deepRed/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  deepRed
+//
+//  Created by Deep Parekh on 9/25/18.
+//  Copyright © 2018 Deep Parekh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
